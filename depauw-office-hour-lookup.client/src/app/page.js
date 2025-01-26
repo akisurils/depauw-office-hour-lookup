c40d05b5dfbe242474f82b199bb997c219e3adcb
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-25 23:21:28 -0500

    feat: add header

--- depauw-office-hour-lookup.client/src/app/page.js
+++ depauw-office-hour-lookup.client/src/app/page.js
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <>
-            <div className="bg"></div>
-            <div className="header"></div>
+        <div className="bg">
+            <div className="header">
+                <div>DePauw | Office Hour Lookup</div>
+            </div>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 Hello world watafak
             </div>
-        </>
+        </div>
     );
 }
