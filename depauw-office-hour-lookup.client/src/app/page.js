02cf5732905e1c3eb67244e85cc8736fdc33cee6
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-25 23:10:47 -0500

    feat: Add background

--- depauw-office-hour-lookup.client/src/app/page.js
+++ depauw-office-hour-lookup.client/src/app/page.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            Hello world
-        </div>
+        <>
+            <div className="bg"></div>
+            <div className="header"></div>
+            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                Hello world watafak
+            </div>
+        </>
     );
 }
