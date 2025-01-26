a5b7653a891df5701e293e35b80b5c6a944514a5
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-26 00:35:29 -0500

    feat: add subheader link

--- depauw-office-hour-lookup.client/src/app/page.js
+++ depauw-office-hour-lookup.client/src/app/page.js
@@ -4,7 +4,12 @@ export default function Home() {
     return (
         <div className="bg">
             <div className="header">
-                <div>DePauw | Office Hour Lookup</div>
+                <div>DEPAUW | OFFICE HOUR LOOKUP</div>
+                <div className="subheader">
+                    <a href="https://github.com/akisurils">SUGGESTION</a>
+                    <div className="separator">|</div>
+                    <a href="https://github.com/akisurils">PROFESSOR</a>
+                </div>
             </div>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 Hello world watafak
