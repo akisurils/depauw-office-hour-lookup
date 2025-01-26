9b4c7b5b373dcbc1d06978ab607e18f06c3118f6
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-25 19:16:36 -0500

    fix: Remove gitignored directory

--- depauw-office-hour-lookup.server/Data/Model.cs
+++
@@ -1,9 +0,0 @@
-
-using static Mysqlx.Datatypes.Scalar.Types;
-
-namespace depauw_officer_hour_lookup.Model {
-    public class OfficeHourModelClass {
-        public int id{get;set;}
-        public String Name{get;set;}
-    }
-}
