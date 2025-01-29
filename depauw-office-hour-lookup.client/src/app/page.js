90afd4e8059feadf5fe321b4cb5352c1e3f85e48
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-29 14:54:10 -0500

    feat: implement the final design of the search bar

--- depauw-office-hour-lookup.client/src/app/page.js
+++ depauw-office-hour-lookup.client/src/app/page.js
@@ -11,6 +11,11 @@ export default function Home() {
                     <a href="https://github.com/akisurils">PROFESSOR</a>
                 </div>
             </div>
+            <div className="searchbar">
+                <div className="search-title">Faculty Name</div>
+                <input type="text" className="search-form" />
+                <button className="search-button">Search</button>
+            </div>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
         </div>
     );
