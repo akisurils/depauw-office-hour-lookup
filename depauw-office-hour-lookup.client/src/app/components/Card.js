804dfb9a99fa36905fe2b476f86a95ae27c6cc9d
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-29 15:21:23 -0500

    feat: create Card component blueprint

--- depauw-office-hour-lookup.client/src/app/components/Card.js
+++ depauw-office-hour-lookup.client/src/app/components/Card.js
@@ -4,10 +4,19 @@ import "./Card.css";
 export const Card = () => {
     return (
         <div className="card">
-            <div>
-                <div>Sect 1</div>
-                <div>Sect 2</div>
-                <div>Sect 3</div>
+            <div className="card-info">
+                <div className="card-profile-pic">
+                    Profile Pic
+                    <div></div>
+                </div>
+                <div className="card-general">
+                    General Info
+                    <div></div>
+                </div>
+                <div className="card-office-hour">
+                    Office Hour
+                    <div></div>
+                </div>
             </div>
             <div className="card-footer"></div>
         </div>
