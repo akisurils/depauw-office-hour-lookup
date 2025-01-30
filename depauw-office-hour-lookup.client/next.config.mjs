ac2ed11f34df333e6d5e58b834309d4a3d184d4b
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-29 21:59:03 -0500

    feat: add default template for card

--- depauw-office-hour-lookup.client/next.config.mjs
+++ depauw-office-hour-lookup.client/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "media.istockphoto.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "my.depauw.edu",
+                port: "",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
--- depauw-office-hour-lookup.client/src/app/components/Card.js
+++ depauw-office-hour-lookup.client/src/app/components/Card.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Card.css";
+import Image from "next/image";
 
 export const Card = () => {
     return (
         <div className="card">
             <div className="card-info">
                 <div className="card-profile-pic">
-                    Profile Pic
-                    <div></div>
+                    <Image
+                        src="https://my.depauw.edu/e/cis/csimages/000726034.jpg"
+                        alt="empty profile pic"
+                        width={40}
+                        height={100}
+                    />
                 </div>
                 <div className="card-general">
                     General Info
