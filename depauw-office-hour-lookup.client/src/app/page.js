dd175bda48bacab480d7c9439f6c0398080ca974
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-02-01 15:58:26 -0500

    feat: add filter placeholder

--- depauw-office-hour-lookup.client/src/app/page.js
+++ depauw-office-hour-lookup.client/src/app/page.js
@@ -17,6 +17,11 @@ export default function Home() {
                 <input type="text" className="search-form" />
                 <button className="search-button">Search</button>
             </div>
+            <div>
+                Filter
+                <div>Department</div>
+                <div>Skin color</div>
+            </div>
             <Card />
         </div>
     );
