bf6dda8ab889607255d277031be6bc25304f2f0e
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-02-01 21:55:49 -0500

    feat: add temporary test controller and model

---
+++ depauw-office-hour-lookup.server/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DePauwOfficeHourLookup.server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DePauwOfficeHourLookup.server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TestInfo> TestInfos { get; set; }
+    }
+}
